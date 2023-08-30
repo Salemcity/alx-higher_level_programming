2d4b4920fb467c26d7ad7e6d520bd61f2dace247
Salemcity <gemgbeminiyi@gmail.com>
2023-08-30 09:47:40 -0700

    5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,17 +1,33 @@
 #!/usr/bin/python3
-Square = __import__('5-square').Square
 
-my_square = Square(3)
-my_square.my_print()
+"""Define a class Square"""
 
-print("--")
 
-my_square.size = 10
-my_square.my_print()
+class Square:
+    """Represent a square"""
+    def __init__(self, size=0):
 
-print("--")
+        self.size = size
 
-my_square.size = 0
-my_square.my_print()
+    @property
+    def size(self):
+        return self.__size
 
-print("--")
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
