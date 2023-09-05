39569d3146e1fe98c02d881b22446ffe59e6e8ee
Salemcity <gemgbeminiyi@gmail.com>
2023-09-05 15:52:29 -0700

    python-everything_is_object

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""A locked class"""
+
+class LockedClass:
+    """A locked class that only lets the user dynamically create the instance
+    attribute 'first_name'"""
+    __slots__ = ["first_name"]
+
+    def __init__(self, first_name=""):
+        self.first_name = first_name
