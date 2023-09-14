7e236b2f9508d2f83fd51e5a65dfc4a068e8e5d5
Salemcity <gemgbeminiyi@gmail.com>
2023-09-13 17:41:20 -0700

    updated

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defining  a write__file function."""
+"""Defining  a write_file function."""
 
 
 def write_file(filename="", text=""):
--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Defining a JSON file-writing function."""
+"""Defining a Json file-writing function."""
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """Write an object to a text file using JSON representation."""
-    with open(filename, "w") as f:
+    """Write an object to a text file using json representation."""
+    with open(filename, "w", encoding="utf-8") as f:
         json.dump(my_obj, f)
 
