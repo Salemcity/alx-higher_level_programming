01a05e828576b2f5bfeaa77ce018bf5d7e233b15
root <gemgbeminiyi@gmail.com>
2023-11-14 15:02:10 -0800

    more update

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const myArgs = process.argv.slice(2);
+if (myArgs.length < 2) {
+  console.log(0);
+} else {
+  myArgs.sort(function (a, b) {
+    return a - b;
+  });
+  console.log(myArgs[myArgs.length - 2]);
+}
