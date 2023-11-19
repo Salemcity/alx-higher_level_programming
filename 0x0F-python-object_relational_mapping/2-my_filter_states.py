e27282989f649ea5c7ed0cf8c37ce84de08d3826
root <gemgbeminiyi@gmail.com>
2023-11-19 12:57:32 -0800

    updated files

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
-"""
-A script that takes in an argument and displays 
-all values in the states table of where `name`
-matches the argument from the 
-database `hbtn_0e_0_usa`.
-"""
-
-import MySQLdb as db
-from sys import argv
+""" A script that takes in an argument and displays 
+all values in the states table """
 
 if __name__ == '__main__':
-    """
-    Get access to the database and get the states
-    from the database.
-    """
-    db_connect = db.connect(host="localhost", port=3306,
-                            user=argv[1], passwd=argv[2], db=argv[3])
-    db_cursor = db_connect.cursor()
 
-    db_cursor.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
-                        states.id ASC".format(argv[4]))
-    rows_selected = db_cursor.fetchall()
+    from sys import argv
+    import MySQLdb
 
-    for row in rows_selected:
-        print(row)
+    db_user = argv[1]
+    db_passwd = argv[2]
+    db_name = argv[3]
+    search = argv[4]
+
+    database = MySQLdb.connect(host='localhost',
+                               port=3306,
+                               user=db_user,
+                               passwd=db_passwd,
+                               db=db_name)
+
+    cursor = database.cursor()
+
+    cursor.execute('SELECT id, name FROM states\
+                   WHERE states.name = \'{}\'\
+                   ORDER BY states.id ASC'.format(search))
+
+    for row in cursor.fetchall():
+        if row[1] == search:
+            print(row)
